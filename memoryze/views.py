34ee48c6e34d699ff3dcc4f0d1849a4e4fbab871
alishalawani <alishalawani@gmail.com>
2020-12-18 20:18:46 -0600

    set up the views for the audio and playlist models

--- memoryze/views.py
+++ memoryze/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
+from .models import Audio, Playlist
+from .serializers import AudioSerializer, PlaylistSerializer
 # Create your views here.
+
+class AudioView(generics.ListCreateAPIView):
+    queryset = Audio.objects.all()
+    serializer_class = AudioSerializer\
+        
+class AudioDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Audio.objects.all()
+    serializer_class = AudioSerializer
+
+class PlaylistView(generics.ListCreateAPIView):
+     queryset = Playlist.objects.all()
+     serializer_class = PlaylistSerializer
+     
+class PlaylistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer 
