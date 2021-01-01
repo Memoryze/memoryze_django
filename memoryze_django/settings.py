75b85bcc2cb5ff4f37ae78c067c49fca3b3508e2
alishalawani <alishalawani@gmail.com>
2020-12-31 21:12:09 -0600

    adding some aws settings

--- memoryze_django/settings.py
+++ memoryze_django/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'multiselectfield',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -158,3 +159,23 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
     'UPDATE_LAST_LOGIN': True,
 }
+
+# AWS SETTINGS
+
+AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET_NAME']
+# AWS_S3_REGION_NAME = 'REGION_NAME'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = os.environ['ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['SECRET_ACCESS_KEY']
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
