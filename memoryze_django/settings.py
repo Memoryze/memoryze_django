d2c77b74c485afc64f1801c2805b97b8bbfa4e67
alishalawani <alishalawani@gmail.com>
2021-01-01 18:52:17 -0600

    added a defalt storage setting

--- memoryze_django/settings.py
+++ memoryze_django/settings.py
@@ -164,7 +164,7 @@ SIMPLE_JWT = {
 }
 
 # AWS SETTINGS
-
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET_NAME']
 # AWS_S3_REGION_NAME = 'REGION_NAME'  # e.g. us-east-2
 AWS_ACCESS_KEY_ID = os.environ['ACCESS_KEY_ID']
@@ -175,6 +175,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
 # you run `collectstatic`).
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+ 
 
 
 # STATICFILES_LOCATION = 'static'
