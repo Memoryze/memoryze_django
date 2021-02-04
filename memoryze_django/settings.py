3df87d122f8e4a8bb60f739947e0daec5ac21774
alishalawani <alishalawani@gmail.com>
2021-02-03 22:34:06 -0600

    added some variables to the settings file for sending account activation email

--- memoryze_django/settings.py
+++ memoryze_django/settings.py
@@ -185,3 +185,9 @@ MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 # STATIC_ROOT=os.path.join(BASE_DIR, "static/")
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
